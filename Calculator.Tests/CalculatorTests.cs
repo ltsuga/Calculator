54a67e61047b5d5287cacf5606b3dcf7e54e4868
ltsuga <85001056+ltsuga@users.noreply.github.com>
2021-08-25 10:23:02 -0300

    Update CalculatorTests.cs
    Additional Test Cases

--- Calculator.Tests/CalculatorTests.cs
+++ Calculator.Tests/CalculatorTests.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Xunit;
 using Shouldly;
+using Xunit;
 
 namespace Calculator.Tests
 {
@@ -21,14 +19,28 @@ namespace Calculator.Tests
             result.ShouldBe(3);
         }
 
-        [Fact]
-        public void Subtract_ShouldReturnTheSubtractionOftheProvidedNumbers()
+        [Theory]
+        [InlineData(0, 1, -1)]
+        [InlineData(1, 0, 1)]
+        [InlineData(1, 1, 0)]
+        public void Subtract_ShouldReturnTheSubtractionOftheProvidedNumbers(int num1, int num2, int expectedAnswer)
         {
             var calculator = new Calculator();
 
-            var result = calculator.Subtract(2, 1);
+            var result = calculator.Subtract(num1,num2);
 
-            result.ShouldBe(1);
+            result.ShouldBe(expectedAnswer);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 2)]
+        [InlineData(1, 0, 0)]
+        [InlineData(-1, 2, -2)]
+        public void Multiple_ShouldReturnTheMultiplicationOfTheProvidedNumbers(int num1, int num2, int expectedAnswer)
+        {
+            var calculator = new Calculator();
+            var result = calculator.Multiply(num1, num2);
+            result.ShouldBe(expectedAnswer);
         }
 
         [Fact]
@@ -37,6 +49,17 @@ namespace Calculator.Tests
             var calculator = new Calculator();
 
             var exception = Should.Throw<Exception>(() => calculator.Divide(2, 0));
+        }
+
+        [Theory]
+        [InlineData(2, 1, 2)]
+        [InlineData(2, -1, -2)]
+        [InlineData(-2, 1, -2)]
+        public void Divide_ShouldReturnTheDivisionOfTheProvidedNumbers(int num1, int num2, int expectedAnswer)
+        {
+            var calculator = new Calculator();
+            var result = calculator.Divide(num1, num2);
+            result.ShouldBe(expectedAnswer);
         }
     }
 }
